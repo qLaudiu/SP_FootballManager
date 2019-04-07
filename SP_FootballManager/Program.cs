5f5d95de90b0dc18184780265285d2c070ed9b01
Claudiu Bugeac <claudiu.bugeac@asperitas-solutions.com>
2019-04-07 14:42:02 +0300

    Initial commit

---
+++ SP_FootballManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SP_FootballManager
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
